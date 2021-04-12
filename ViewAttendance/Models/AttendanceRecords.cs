c37ada27b0dbd46d4ab4d10e33330b3b2bf6d6fa
Shivam Magotra <66984554+sm4107@users.noreply.github.com>
2021-04-12 20:59:01 +0530

    First Commit

---
+++ ViewAttendance/Models/AttendanceRecords.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ViewAttendance.Models
+{
+    public class AttendanceRecords
+    {
+		
+	    [Key]
+	    public int AttendanceID { get; set; }
+		public int ProjectID { get; set; }
+		public System.Int64 EmployeeID { get; set; }
+		public DateTime AttedanceDate { get; set; }
+		public System.TimeSpan InTime { get; set; }
+		public System.TimeSpan OutTime { get; set; }
+
+		public string AttendanceStatus { get; set; }
+		public DateTime StatusUpdateDate { get; set; }
+		public System.Int64 StatusUpdatedBy { get; set; }
+
+
+	}
+}
---
+++ ViewAttendance/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ViewAttendance.Models;
+
+
+namespace ViewAttendance.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _adb;
+        public IndexModel(ApplicationDbContext adb)
+        {
+            _adb = adb;
+        }
+        public IEnumerable<AttendanceRecords> results { get; set; }
+        
+        public void OnGet()
+        {
+            results = _adb.TRAttendanceDetails.ToList();
+        }
+        public void OnPost(DateTime startdate , DateTime enddate)
+        {
+            results = (from x in _adb.TRAttendanceDetails where (x.AttedanceDate >= startdate) && (x.AttedanceDate <= enddate) select x).ToList();
+        }
+    }
+}
